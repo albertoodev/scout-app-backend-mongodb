9d89429780b3ad624bd8b085e47d37c1e576968e
albertoodev <amouraammarabderafik@gmail.com>
2023-11-22 02:27:38 +0300

    user model v1.1

--- api/auth/models/user.ts
+++ api/auth/models/user.ts
@@ -1,25 +1,69 @@
 import mongoose, { Document, Schema } from "mongoose";
-import { IScout } from "../../scout/models/scout";
+import { emailRegExp } from "../../../utils/reg-exp/reg-exp";
+
 interface IUser extends Document {
-  id: string;
   firstName: string;
   lastName: string;
   email: string;
   password: string;
   phone: string;
-  role: UserRole;
+  role: string;
   bio?: string;
-  children: mongoose.Types.ObjectId[] | IScout[];
+  children?: mongoose.Types.ObjectId[];
   createdAt: Date;
-  updatedAt: Date;
+  updatedAt?: Date;
 }
 
-enum UserRole {
-  admin = "admin",
-  leader = "leader",
-  parent = "parent",
-}
+const UserSchema: Schema<IUser> = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 20,
+  },
+  lastName: {
+    type: String,
+    required: [true, "Please provide a last name"],
+  },
+  email: {
+    type: String,
+    unique: true,
+    lowercase: true,
+    match: [emailRegExp, "Please provide a valid email"],
+  },
+  password: {
+    type: String,
+    required: [true, "Please provide a password"],
+    select: false,
+  },
+  phone: {
+    type: String,
+    required: [true, "Please provide a phone number"],
+    unique: true,
+  },
+  role: {
+    type: String,
+    enum: ["admin", "leader", "parent"],
+    default: "parent",
+  },
+  bio: {
+    type: String,
+  },
+  children: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Scout",
+    },
+  ],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+  },
+});
 
-export {
-  IUser,
-};
+const User = mongoose.model<IUser>("User", UserSchema);
+
+export { User };
