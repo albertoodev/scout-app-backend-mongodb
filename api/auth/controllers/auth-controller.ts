ff20ff8c84679f9b858030653b97e5b4fc3ab34c
albertoodev <amouraammarabderafik@gmail.com>
2023-11-22 06:53:55 +0300

    init auth and registration controllers

---
+++ api/auth/controllers/auth-controller.ts
@@ -0,0 +1,41 @@
+import { Request, Response } from "express";
+import { createCustomError } from "../../../utils/errors/custom-error";
+
+const login = async (req: Request, res: Response): Promise<void> => {
+  throw createCustomError("Not implemented", 501);
+};
+
+// registration
+const verifyEmail = async (req: Request, res: Response): Promise<void> => {
+  throw createCustomError("Not implemented", 501);
+};
+
+/// this method is for checking the verification code for email and forgot password
+const checkVerificationCode = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  throw createCustomError("Not implemented", 501);
+};
+
+const register = async (req: Request, res: Response): Promise<void> => {
+  throw createCustomError("Not implemented", 501);
+};
+
+// forgot password
+const forgotPassword = async (req: Request, res: Response): Promise<void> => {
+  throw createCustomError("Not implemented", 501);
+};
+
+const resetPassword = async (req: Request, res: Response): Promise<void> => {
+  throw createCustomError("Not implemented", 501);
+};
+
+export default {
+  login,
+  verifyEmail,
+  checkVerificationCode,
+  register,
+  forgotPassword,
+  resetPassword,
+};
---
+++ api/auth/controllers/registeration-code-controller.ts
@@ -0,0 +1,46 @@
+import { Request, Response } from "express";
+import { createCustomError } from "../../../utils/errors/custom-error";
+
+const createRegistrationCode = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  throw createCustomError("Not implemented", 501);
+};
+
+const verifyRegistrationCode = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  throw createCustomError("Not implemented", 501);
+};
+
+const getRegistrationCodes = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  throw createCustomError("Not implemented", 501);
+};
+
+/// this method is for updating the rule of the registration code or the usage of it (used or not)
+const updateRegistrationCode = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  throw createCustomError("Not implemented", 501);
+};
+
+const deleteRegistrationCode = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  throw createCustomError("Not implemented", 501);
+};
+
+export default {
+  createRegistrationCode,
+  verifyRegistrationCode,
+  getRegistrationCodes,
+  updateRegistrationCode,
+  deleteRegistrationCode,
+};
