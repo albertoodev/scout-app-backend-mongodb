099c1266a64af89c1bf6520bf87f39511c36c577
albertoodev <amouraammarabderafik@gmail.com>
2023-11-27 09:24:23 +0300

    get all users user-controller

--- api/v1/auth/controllers/user-controller.ts
+++ api/v1/auth/controllers/user-controller.ts
@@ -1,8 +1,15 @@
 import { Request, Response } from "express";
 import { createCustomError } from "../../../../utils/errors/custom-error";
+import userService from "../services/user-service";
+import { filterValidProperties } from "../../../../utils/utils";
 
-const getAllUsers = async (req: Request, res: Response): Promise<void> => {
-  throw createCustomError("Not implemented", 501);
+const getAll = async (req: Request, res: Response): Promise<void> => {
+  const filter = filterValidProperties(["role", "code"], req.query);
+  const users = await userService.getAll(filter);
+  if (!users) {
+    throw createCustomError("Users not found", 404);
+  }
+  res.status(200).json(users);
 };
 
 const getUser = async (req: Request, res: Response): Promise<void> => {
@@ -18,7 +25,7 @@ const deleteUser = async (req: Request, res: Response): Promise<void> => {
 };
 
 export default {
-  getAllUsers,
+  getAll,
   getUser,
   updateUser,
   deleteUser,
